2d9d5c62458d6db653a54d85f105619b3945a15d
dehemi weerakoon <115975647+dehemiweerakoon@users.noreply.github.com>
2025-03-16 13:39:39 +0530

    added changes #02

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,20 +12,34 @@ import useTranslate from "@/hooks/useTranslate";
 import LanguageSelector from "../components/inputs/LanguageSelector"
 import SvgDecorations from "@/components/SvgDecorations";
 import CategoryLinks from "@/components/categoryLinks";
-const Home:React.FC=()=> {
+const Home: React.FC = () => {
   const [sourceText, setSourceText] = useState<string>("");
-  const[copied,setCopied] = useState<boolean>(false);
+  const [copied, setCopied] = useState<boolean>(false);
   const [favorite, setFavorite] = useState<boolean>(false);
-  const [languages] = useState<string[]>(["English","French","Spanish","Sinhala","Tamil"])
+  const [languages] = useState<string[]>(["Russian", "French", "Spanish", "Sinhala", "Italian", "German", "Japanese", "Chinese", "Korean", "Arabic"])
   const [selectedLanguage, setSelectedLanguage] = useState<string>("Sinhala");
-  const targetText= useTranslate(sourceText, selectedLanguage);
+  const targetText = useTranslate(sourceText, selectedLanguage);
+
+  const languageMap: { [key: string]: string } = {
+    "Russian": "ru-RU",
+    "French": "fr-FR",
+    "Spanish": "es-ES",
+    "Sinhala": "si-LK",
+    "Italian": "it-IT",
+    "German": "de-DE",
+    "Japanese": "ja-JP",
+    "Chinese": "zh-CN",
+    "Korean": "ko-KR",
+    "Arabic": "ar-SA",
+  };
 
   const handleAudioPlayBack = (sourceText: string) => {
     const audio = new SpeechSynthesisUtterance(sourceText);
+    audio.lang = languageMap[selectedLanguage];
     window.speechSynthesis.speak(audio);
   };
 
-  const handlePaste = (text: string) => {};
+  const handlePaste = (text: string) => { };
 
   const handleFileUpload = (event: ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
@@ -41,35 +55,35 @@ const Home:React.FC=()=> {
     }
   };
   function handleCopyToClipboard() {
-   // throw new Error("Function not implemented.");
-    navigator.clipboard.writeText(targetText+"");
+    // throw new Error("Function not implemented.");
+    navigator.clipboard.writeText(targetText + "");
     setCopied(true);
-    setTimeout(()=>{
+    setTimeout(() => {
       setCopied(false);
-    },2000);
+    }, 2000);
 
   }
-  function handleFavorite(){
+  function handleFavorite() {
     setFavorite(!favorite);
   }
 
   return (
     <>
-       <div className="w-full bg-white bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center">
-      <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+      <div className="w-full bg-dark bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center">
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-dark [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
 
-      <div className="relative overflow-hidden h-screen">
+        <div className="relative  h-screen">
           <div className="max-w-[85rem] mx-auto px-4 sm:px-6 py-10 sm:py-24">
             <div className="text-center">
-              <h1 className="text-4xl sm:text-6xl font-bold">
-                <span className="text-blue-900">Grammar</span>{" "}
+              <h1 className="text-3xl sm:text-6xl font-bold">
+                <span className="text-blue-900">Grammar</span>
                 <span className="text-blue-400">Speaker</span>
               </h1>
-              <p className="mt-3 text-neutral-500">
+              <p className="mt-1 text-neutral-500">
                 Grammar Speaker : Give Suggestions , Resolving Issues
               </p>
-              <div className="mt-7 sm:mt-12 mx-auto max-w-3xl relative">
-                <div className="grid gap-6 md:grid-cols-2 grid-cols-1">
+              <div className="mt-7 sm:mt-12 mx-auto max-w-3xl relative mb-5">
+                <div className="grid gap-6 md:grid-cols-1 grid-cols-1">
                   <div className="relative z-10 flex flex-col space-x-3 border rounded-lg shadow-lg bg-neutral-900 border-neutral-700 shadow-gray-900/20">
                     <TextArea
                       id="source-language"
@@ -98,28 +112,28 @@ const Home:React.FC=()=> {
                     </div>
                   </div>
                   <div className="relative z-10 flex flex-col space-x-3 border rounded-lg shadow-lg bg-neutral-900 border-neutral-700 shadow-gray-900/20">
-                  <TextArea id={'target-language'} value={targetText} onChange={()=>{}} placeholder={"target-language"}/>
-                  <div className="flex flex-row justify-between w-full">
-                    <span className="curser-pointer flex space-x-2 flex-row items-center">
-                      <LanguageSelector selectedLanguage={selectedLanguage} setSelectedLanguage={setSelectedLanguage} language={languages}/>
-                    <IconVolume size={22} onClick={() => handleAudioPlayBack(targetText+"")} />
-                    </span>
-                    <div className="flex flex-row items-center space-x-2 pr-4 cursor-pointer">
-                      <IconCopy size={22} onClick={handleCopyToClipboard}/>
-                      {copied&& <span className="text-xs text-green-500">Copied</span>}
-                    <IconThumbUp size={22}/>
-                    <IconThumbDown size={22}/>
-                    <IconStar size={22} onClick={handleFavorite} className={favorite ?"text-yellow-500":""}/>
+                    <TextArea id={'target-language'} value={targetText} onChange={() => { }} placeholder={"target-language"} />
+                    <div className="flex flex-row justify-between w-full">
+                      <span className="curser-pointer flex space-x-2 flex-row items-center">
+                        <LanguageSelector selectedLanguage={selectedLanguage} setSelectedLanguage={setSelectedLanguage} language={languages} />
+                        <IconVolume size={22} onClick={() => handleAudioPlayBack(targetText + "")} />
+                      </span>
+                      <div className="flex flex-row items-center space-x-2 pr-4 cursor-pointer">
+                        <IconCopy size={22} onClick={handleCopyToClipboard} />
+                        {copied && <span className="text-xs text-green-500">Copied</span>}
+                        <IconThumbUp size={22} />
+                        <IconThumbDown size={22} />
+                        <IconStar size={22} onClick={handleFavorite} className={favorite ? "text-yellow-500" : ""} />
+                      </div>
                     </div>
                   </div>
-                  </div>
                 </div>
-                <SvgDecorations/>
+                <SvgDecorations />
               </div>
-              <CategoryLinks/>
+            <CategoryLinks />
             </div>
           </div>
-        </div>
+        </div>  
       </div>
     </>
   );
